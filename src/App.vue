0d3b6cf443f4cb418388bfbf3228e831b255bfb4
User <zaleshin99@mail.ru>
2024-01-29 17:00:31 +0300

    v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
 <template>
+  <div class="appHeader">
+    <div><a-space direction="vertical">
+      <a-space warp>
+        <a-tooltip title="search">
+          <a-button type="primary" shape="circle" :icon="h(SearchOutlined)" />
+        </a-tooltip>
+        <a-button type="primary" shape="circle">A</a-button>
+        <a-button type="primary" :icon="h(SearchOutlined)">Search</a-button>
+        <a-tooltip title="search">
+          <a-button shape="circle" :icon="h(SearchOutlined)" />
+        </a-tooltip>
+        <a-button :icon="h(SearchOutlined)">Search</a-button>
+      </a-space>
+      <a-space warp>
+        <a-tooltip title="search">
+          <a-button shape="circle" :icon="h(SearchOutlined)" />
+        </a-tooltip>
+        <a-button :icon="h(SearchOutlined)">Search</a-button>
+        <a-tooltip title="search">
+          <a-button type="dashed" shape="circle" :icon="h(SearchOutlined)" />
+        </a-tooltip>
+        <a-button type="dashed" :icon="h(SearchOutlined)">Search</a-button>
+        <a-button :icon="h(SearchOutlined)" href="https://www.google.com" />
+      </a-space>
+    </a-space></div>
+    <div></div>
+  </div>
+
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script  setup>
+import { h } from 'vue';
+import { SearchOutlined } from '@ant-design/icons-vue';
+</script>
+
 
 <style lang="scss">
 #app {
@@ -19,12 +53,18 @@ nav {
   padding: 30px;
 
   a {
+
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+
+  .appHeader {
+    display: flex;
+    padding: var(--base-size-16, 16px);
+    gap: var(--base-size-12, 12px);
   }
 }
 </style>
