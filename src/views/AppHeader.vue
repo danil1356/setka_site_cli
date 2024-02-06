89e424293306af37e192e688c9065fa8cc1e4e92
User <zaleshin99@mail.ru>
2024-02-06 16:10:57 +0300

    v0.1

---
+++ src/views/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script setup>
+import { onMounted, onUpdated, onUnmounted } from 'vue'
+export default {
+ setup(){
+
+
+ }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
